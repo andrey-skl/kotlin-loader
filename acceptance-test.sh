51c10ff076691a6c288b7f1664c38302ec5b7354
Andrey Skladchikov <andrey.skladchikov@gmail.com>
2016-07-01 00:36:34 +0300

    Done simple acceptance test

--- acceptance-test.sh
+++ acceptance-test.sh
@@ -1,4 +1,4 @@
 # Simple test
-npm run example-simple
+npm run compile-simple
 
-cmp examples/simple/annotated-reference.js examples/simple/dist/build.js && echo 'Simple test passed' || exit 123
+cmp examples/simple/compiled-reference.js examples/simple/dist/build.js && echo 'Simple test passed' || exit 123
