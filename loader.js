9f5da7418c3bd82c4a097eca2b077fbb866df001
Andrey.Skladchikov <andrey.skladchikov@jetbrains.com>
2016-08-13 13:25:22 +0300

    Throw readable error if async mode is not enabled

--- loader.js
+++ loader.js
@@ -4,6 +4,11 @@ var kotlinCopiler = require('./compiler/kotlin-compiler');
 module.exports = function () {
     this.cacheable();
     var callback = this.async();
+
+    if (!callback) {
+        throw 'webpack-kotlin-loader currently only supports async mode.';
+    }
+
     var filename = loaderUtils.getRemainingRequest(this);
     this.addDependency(filename);
 
