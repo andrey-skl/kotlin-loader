eac5be8c74971c5bbf9c2871fd14d3be57b5a2d0
Andrey.Skladchikov <andrey.skladchikov@jetbrains.com>
2017-04-15 03:03:34 +0300

    Update webpack to 2.4

--- examples/simple/compiled-reference.js
+++ examples/simple/compiled-reference.js
@@ -25,12 +25,11 @@ webpackJsonp([0],[
 
 
 /***/ }),
-/* 2 */,
-/* 3 */
+/* 2 */
 /***/ (function(module, exports, __webpack_require__) {
 
 __webpack_require__(1);
 
 /***/ })
-],[3]);
+],[2]);
 //# sourceMappingURL=build.js.map
